4dc3f7f5b761574b3b56dbf0f55476e7844f4c8c
Takahiro-Asano <t-asano@iis.u-tokyo.ac.jp>
2018-12-14 18:00:47 +0900

    controller UI added

--- src/control_UI.py
+++ src/control_UI.py
@@ -2,15 +2,15 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-rospy.init_ndoe('vel_publisher')
-pub = rospy.publisher('/armed3w/diff_drive_controller/cmd_vel', Twist, quesize = 10)
+rospy.init_node('vel_publisher')
+pub = rospy.Publisher('/armed3w/diff_drive_controller/cmd_vel', Twist, queue_size = 10)
 while not rospy.is_shutdown():
 	vel = Twist()
 	direction = raw_input('f: forward, b: back, l:left, r:right > ')
 	if 'f' in direction:
-		vel.linear.x = 0.5
+		vel.linear.x = 2.0
 	if 'b' in direction:
-		vel.linear.x = -0.5
+		vel.linear.x = -2.0
 	if 'l' in direction:
 		vel.angular.z = 1.0
 	if 'r' in direction:
